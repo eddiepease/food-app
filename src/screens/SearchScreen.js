097b7c60c9219d3272c0b7b37229a839ea485c3d
Eddie Pease <eddie.pease@sanome.com>
2022-07-23 16:59:46 +0100

    Tidy up: replace view with null element, add some bottom margin on the search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -16,20 +16,19 @@ const SearchScreen = () => {
     }
 
     return (
-        <View style={{flex: 1}}>
+        <>
             <SearchBar 
                 term={term} 
                 onTermChange={setTerm}
                 onTermSubmit={() => searchApi(term)}
                 />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results</Text>
             <ScrollView>
                 <ResultsList title="Cost Effective" results={filterResultsByPrice('$')}/>
                 <ResultsList title="Bit Pricier" results={filterResultsByPrice('$$')}/>
                 <ResultsList title="Big Spender" results={filterResultsByPrice('$$$')}/>
             </ScrollView>
-        </View>
+        </>
     )
 }
 
