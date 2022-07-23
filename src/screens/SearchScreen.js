7789461f378ca4f3b16a3041a9672e9de0681442
Eddie Pease <eddie.pease@sanome.com>
2022-07-23 16:54:04 +0100

    Add Restaurant Search Results to main Search Screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/resultsList";
 
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        })
+    }
+
     return (
-        <View>
+        <View style={{flex: 1}}>
             <SearchBar 
                 term={term} 
                 onTermChange={setTerm}
@@ -17,6 +24,11 @@ const SearchScreen = () => {
                 />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ScrollView>
+                <ResultsList title="Cost Effective" results={filterResultsByPrice('$')}/>
+                <ResultsList title="Bit Pricier" results={filterResultsByPrice('$$')}/>
+                <ResultsList title="Big Spender" results={filterResultsByPrice('$$$')}/>
+            </ScrollView>
         </View>
     )
 }
