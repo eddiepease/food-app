db602507492356c728367d7b2d414b740b70bb51
Eddie Pease <eddie.pease@sanome.com>
2022-07-23 15:39:20 +0100

    Add Error Handling for API

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,8 +7,10 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async () => {
+        try {
         const response = await yelp.get('/search', {
             params: {
                 limit: 50,
@@ -17,8 +19,12 @@ const SearchScreen = () => {
             }
         });
         setResults(response.data.businesses);
+        }
+        catch (e) {
+            setErrorMessage('Something went wrong!')
+        }
     }
-    
+
     return (
         <View>
             <SearchBar 
@@ -26,6 +32,7 @@ const SearchScreen = () => {
                 onTermChange={setTerm}
                 onTermSubmit={searchApi}
                 />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
         </View>
     )
