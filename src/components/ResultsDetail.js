fc4292213152753a1594c6025c1d4b17a5aaf250
Eddie Pease <eddie.pease@sanome.com>
2022-07-24 19:02:23 +0100

    Apply Prettier Code Formatting

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
 
-
-const ResultsDetail = ({result}) => {
-    return (
-        <View style={styles.container}>
-            <Image style={styles.image} source={{uri: result.image_url}}/>
-            <Text style={styles.name}>{result.name}</Text>
-            <Text style={styles.detail}>{result.rating} Stars, {result.review_count} Reviews</Text>
-        </View>
-    )
-}
+const ResultsDetail = ({ result }) => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text style={styles.detail}>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        marginLeft: 10
-    },
-    image: {
-        width: 250,
-        height: 120,
-        borderRadius: 4,
-        marginLeft: 10,
-        marginBottom: 5
-    },
-    name: {
-        fontWeight: 'bold',
-        marginLeft: 10
-    },
-    detail: {
-        marginLeft: 10
-    }
+  container: {
+    marginLeft: 10,
+  },
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+    marginLeft: 10,
+    marginBottom: 5,
+  },
+  name: {
+    fontWeight: "bold",
+    marginLeft: 10,
+  },
+  detail: {
+    marginLeft: 10,
+  },
 });
 
 export default ResultsDetail;
